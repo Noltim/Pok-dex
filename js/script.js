dd73d9544b2b759b1d89202063d9869b267fa03a
Milton Oliveira <milton_oliveira18@live.com>
2023-10-17 16:18:27 -0300

    Consumo dos nomes, ids e gifs dos pokemons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form')
+const inputSearch = document.querySelector('.input__search')
 
 
 const fetchPokemon = async (pokemon) => {
@@ -13,6 +17,13 @@ const renderPokemon = async (pokemon) => {
 
     pokemonName.innerHTML = data.name
     pokemonNumber.innerHTML = data.id
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
-renderPokemon('6')
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    renderPokemon(inputSearch.value)
+
+});
