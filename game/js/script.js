0bffbcc5ecd250751c923f0e4667a2d7ba5ab2a2
José Milton de Oliveira Neto <milton_oliveira18@live.com>
2023-10-31 22:25:01 -0300

    Finalização do game

--- game/js/script.js
+++ game/js/script.js
@@ -1,4 +1,5 @@
 const pokemon = document.querySelector('.pokemon');
+const pokeball = document.querySelector('.pokeball');
 
 const jump = () => {
     pokemon.classList.add('jump');
@@ -7,5 +8,27 @@ const jump = () => {
         pokemon.classList.remove('jump');
     }, 500)
 }
+
+const loop = setInterval(() => {
+
+    const pokeballPosition = pokeball.offsetLeft;
+    const pokemonPosition = +window.getComputedStyle(pokemon).bottom.replace('px','');
+
+    if(pokeballPosition <= 120 && pokeballPosition > 0 && pokemonPosition < 60){
+
+        pokeball.style.animation = 'none';
+        pokeball.style.left = `${pokeballPosition}px`;
+
+        pokemon.style.animation = 'none';
+        pokemon.style.bottom = 0;
+
+        pokemon.src = './imagens/pikaCapturada.gif';
+        pokemon.style.width = '100%'
+        pokemon.style.height = '100%'
+
+        clearInterval(loop);
+    }
+
+},10)
 
 document.addEventListener('keydown', jump);
